58d5d2900fe3dd82c53fc6e89d06989e7876dcaf
duckycoding-dev <duckycoding.dev@gmail.com>
2025-02-09 20:06:33 +0100

    fix configs, add package json script, export validation logic for workspaces options

--- scripts/monorepo_scaffolding/src/index.ts
+++ scripts/monorepo_scaffolding/src/index.ts
@@ -1,8 +1,9 @@
-console.log("Hello via Bun!");import { parseArgs, types } from 'node:util';
-import configs, { type Workspace } from './utils/config_setup';
-import { $ } from 'bun';
+import { parseArgs } from 'node:util';
+import type { Workspace } from './utils/config_setup';
+import configs from './utils/config_setup';
+import { verifyAndFormatWorkspaces } from './utils/cli_args';
 
-const { values: cliArgs, positionals } = parseArgs({
+const { values: cliArgs } = parseArgs({
   args: Bun.argv,
   options: {
     name: {
@@ -52,83 +53,7 @@ const { values: cliArgs, positionals } = parseArgs({
 const scaffoldNPMWorkspacesMonorepo = async () => {
   console.time('davidetest');
   console.log('🚀 Scaffolding Monorepo...');
-  const mapWorkspaceAndPackages: Map<string, string[]> = new Map();
-  const splittedWorkspacePackageNames: [string, string][] = [];
-  cliArgs.workspaces.forEach((workspace) => {
-    if (workspace.at(-1) === '/') {
-      workspace = workspace.slice(0, -1);
-    }
-    if (workspace[0] === '/') {
-      workspace = workspace.slice(1);
-    }
-    const splittedWorkspacePackageName = workspace.split('/');
-    if (splittedWorkspacePackageName.length > 2) {
-      throw new Error(`Invalid workspace name: ${workspace}; workspaces names should not contain more than one "/"`);
-    }
-    const [splittedWorkspace, pkg] = splittedWorkspacePackageName;
-    splittedWorkspacePackageNames.push([splittedWorkspace, pkg]);
-  });
-
-  const workspacesWithoutPackages = new Set<string>();
-  const workspacesWithPackages = new Map<string, string[]>();
-  const workspacesDefinedAsStandaloneAndWithPackages: string[] = [];
-  const workspacesDefinedMultipleTimes: string[] = [];
-  splittedWorkspacePackageNames.forEach(([singleWorkspace, pkg]) => {
-    if (pkg) {
-      if (workspacesWithoutPackages.has(singleWorkspace)) {
-        workspacesDefinedAsStandaloneAndWithPackages.push(singleWorkspace);
-      }
-      const pkgArray = workspacesWithPackages.get(singleWorkspace);
-      if (pkgArray?.includes(pkg)) {
-        // duplicate package name
-        workspacesDefinedMultipleTimes.push(`${singleWorkspace}/${pkg}`);
-      }
-      workspacesWithPackages.set(singleWorkspace, pkgArray ? pkgArray.concat(pkg) : [pkg]);
-    } else {
-      if (workspacesWithPackages.has(singleWorkspace)) {
-        workspacesDefinedAsStandaloneAndWithPackages.push(singleWorkspace);
-      }
-      if (!workspacesWithoutPackages.has(singleWorkspace)) {
-        workspacesWithoutPackages.add(singleWorkspace);
-      } else {
-        workspacesDefinedMultipleTimes.push(singleWorkspace);
-      }
-    }
-  });
-
-  let errorMessage = '';
-  if (workspacesDefinedMultipleTimes.length > 0) {
-    let messageList = '';
-    new Set(workspacesDefinedMultipleTimes).forEach((workspace) => {
-      messageList += `  - ${workspace}\n`;
-    });
-    errorMessage += `Invalid workspace names: you defined the following workspaces multiple times:\n
-${messageList}\n`;
-  }
-  if (workspacesDefinedAsStandaloneAndWithPackages.length > 0) {
-    let messageList = '';
-    new Set(workspacesDefinedAsStandaloneAndWithPackages).forEach((workspace) => {
-      messageList += `  - ${workspace}\n`;
-      const pkgs = workspacesWithPackages.get(workspace);
-      pkgs?.forEach((pkg) => {
-        messageList += `  - ${workspace}/${pkg}\n`;
-      });
-    });
-    errorMessage += `Invalid workspace names: you defined some workspaces both with and without a package, please either define multiple workspaces with each a package or only one workspace without any package\nWorkspaces defined both with and without packages:\n
-${messageList}\n`;
-  }
-  if (errorMessage) {
-    throw new Error(errorMessage);
-  }
-
-  splittedWorkspacePackageNames.forEach(([singleWorkspace, pkg]) => {
-    const itemInMap = mapWorkspaceAndPackages.get(singleWorkspace);
-    if (itemInMap) {
-      if (pkg) itemInMap.push(pkg);
-    } else {
-      mapWorkspaceAndPackages.set(singleWorkspace, pkg ? [pkg] : []);
-    }
-  });
+  const mapWorkspaceAndPackages = verifyAndFormatWorkspaces(cliArgs.workspaces);
   const workspaces: Workspace[] = Array.from(mapWorkspaceAndPackages).map(([name, packages]) => ({ name, packages }));
   console.log('Workspaces:', workspaces);
   throw 'stop';
--- scripts/monorepo_scaffolding/src/utils/cli_args.ts
+++ scripts/monorepo_scaffolding/src/utils/cli_args.ts
@@ -1,1 +1,82 @@
-export function verifyWorkspaces(workspaces: string[]) {}
+export function verifyAndFormatWorkspaces(workspaces: string[]) {
+  const mapWorkspaceAndPackages: Map<string, string[]> = new Map();
+  const splittedWorkspacePackageNames: [string, string | undefined][] = [];
+  workspaces.forEach((workspace) => {
+    if (workspace.at(-1) === '/') {
+      workspace = workspace.slice(0, -1);
+    }
+    if (workspace[0] === '/') {
+      workspace = workspace.slice(1);
+    }
+    const splittedWorkspacePackageName = workspace.split('/');
+    if (splittedWorkspacePackageName.length > 2) {
+      throw new Error(`Invalid workspace name: ${workspace}; workspaces names should not contain more than one "/"`);
+    }
+    
+    const [splittedWorkspace, pkg] = splittedWorkspacePackageName as [string, string | undefined];
+    splittedWorkspacePackageNames.push([splittedWorkspace, pkg]);
+  });
+
+  const workspacesWithoutPackages = new Set<string>();
+  const workspacesWithPackages = new Map<string, string[]>();
+  const workspacesDefinedAsStandaloneAndWithPackages: string[] = [];
+  const workspacesDefinedMultipleTimes: string[] = [];
+  splittedWorkspacePackageNames.forEach(([singleWorkspace, pkg]) => {
+    if (pkg) {
+      if (workspacesWithoutPackages.has(singleWorkspace)) {
+        workspacesDefinedAsStandaloneAndWithPackages.push(singleWorkspace);
+      }
+      const pkgArray = workspacesWithPackages.get(singleWorkspace);
+      if (pkgArray?.includes(pkg)) {
+        // duplicate package name
+        workspacesDefinedMultipleTimes.push(`${singleWorkspace}/${pkg}`);
+      }
+      workspacesWithPackages.set(singleWorkspace, pkgArray ? pkgArray.concat(pkg) : [pkg]);
+    } else {
+      if (workspacesWithPackages.has(singleWorkspace)) {
+        workspacesDefinedAsStandaloneAndWithPackages.push(singleWorkspace);
+      }
+      if (!workspacesWithoutPackages.has(singleWorkspace)) {
+        workspacesWithoutPackages.add(singleWorkspace);
+      } else {
+        workspacesDefinedMultipleTimes.push(singleWorkspace);
+      }
+    }
+  });
+
+  let errorMessage = '';
+  if (workspacesDefinedMultipleTimes.length > 0) {
+    let messageList = '';
+    new Set(workspacesDefinedMultipleTimes).forEach((workspace) => {
+      messageList += `  - ${workspace}\n`;
+    });
+    errorMessage += `Invalid workspace names: you defined the following workspaces multiple times:\n
+${messageList}\n`;
+  }
+  if (workspacesDefinedAsStandaloneAndWithPackages.length > 0) {
+    let messageList = '';
+    new Set(workspacesDefinedAsStandaloneAndWithPackages).forEach((workspace) => {
+      messageList += `  - ${workspace}\n`;
+      const pkgs = workspacesWithPackages.get(workspace);
+      pkgs?.forEach((pkg) => {
+        messageList += `  - ${workspace}/${pkg}\n`;
+      });
+    });
+    errorMessage += `Invalid workspace names: you defined some workspaces both with and without a package, please either define multiple workspaces with each a package or only one workspace without any package\nWorkspaces defined both with and without packages:\n
+${messageList}\n`;
+  }
+  if (errorMessage) {
+    throw new Error(errorMessage);
+  }
+
+  splittedWorkspacePackageNames.forEach(([singleWorkspace, pkg]) => {
+    const itemInMap = mapWorkspaceAndPackages.get(singleWorkspace);
+    if (itemInMap) {
+      if (pkg) itemInMap.push(pkg);
+    } else {
+      mapWorkspaceAndPackages.set(singleWorkspace, pkg ? [pkg] : []);
+    }
+  });
+
+  return mapWorkspaceAndPackages;
+}
